dc26ceb81bcb126763c797f62a298d68778212a0
Kevin Chen <kvn.c8888@gmail.com>
2024-09-01 16:30:07 -0400

    Add .gitignore and refactor transcription function

--- getTranscript.py
+++ getTranscript.py
@@ -2,11 +2,21 @@ import os
 from groq import Groq
 
 client = Groq()
+client.api_key = os.environ["GROQ_API_KEY"]
 filename = "tmp/output.mp3"
 
-with open(filename, "rb") as file:
-    transcription = client.audio.transcriptions.create(
-      file=(filename, file.read()),
-      model="whisper-large-v3",
-      temperature=0.0  # Optional
-    )
+def transcribe_audio():
+    with open(filename, "rb") as file:
+        transcription = client.audio.transcriptions.create(
+            file=(filename, file.read()),
+            model="whisper-large-v3",
+            temperature=0.0,  # Optional
+            # response_format="json",  # Optional
+            language="en",  # Optional 
+        )
+
+    with open("tmp/transcript.txt", "w") as file:
+        file.write(transcription.text)
+
+if __name__ == "__main__":
+    print(transcribe_audio())
