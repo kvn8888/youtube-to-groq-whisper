2e1133d185b016b7c7bcdbb21fffa45b5c491d73
Kevin Chen <kvn.c8888@gmail.com>
2024-09-01 16:05:12 -0400

    Update audio download options and add main function

--- getAudio.py
+++ getAudio.py
@@ -2,13 +2,19 @@ import yt_dlp
 
 options = {
     'format': 'bestaudio/best',
-    'outtmpl': 'tmp/output.mp3',
+    'outtmpl': 'youtube-to-groq-whisper/tmp/output',
+    'keepvideo': True,
+    'nooverwrites': False,
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
-        'preferredquality': '0',
+        'preferredquality': '0'
     }],
 }
 
 def youtube_download(video_url):
     yt_dlp.YoutubeDL(options).download([video_url])
+
+if __name__ == "__main__":
+    url = input("Enter the URL of the YouTube video: ")
+    youtube_download(url)
